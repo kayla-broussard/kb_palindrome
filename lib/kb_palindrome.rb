17f5f1b9d36533b60999bf3560c6f0788339b61c
Kayla Broussard <kbroussardco@gmail.com>
2020-01-13 17:34:27 +0000

    Adds integers to palindrome testing - version 0.2.0.

--- lib/kb_palindrome.rb
+++ lib/kb_palindrome.rb
@@ -1,16 +1,28 @@
 require "kb_palindrome/version"
 
-class String
+module KbPalindrome
 
   # Returns true for a palindrome, false otherwise.
   def palindrome?
-    processed_content == processed_content.reverse
+    if processed_content.empty?
+      false
+    else
+      processed_content == processed_content.reverse
+    end
   end
 
   private
 
     # Returns content for palindrome testing.
     def processed_content
-      self.scan(/[a-z]/i).join.downcase
+      self.to_s.scan(/[a-z0-9]/i).join.downcase
     end
+end
+
+class String
+  include KbPalindrome
+end
+
+class Integer
+  include KbPalindrome
 end
